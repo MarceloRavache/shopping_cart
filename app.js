abcc79f186b385d383acb0fdeee222de0c3e820e
MarceloRavache <jm.ravache@outlook.com>
2020-06-25 01:19:07 -0300

    adicionado conexão com o banco de dados e route criadas

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const path = require('path');
 const config = require('./configs/config');
+const config_database = require('./configs/database');
+const mongoose = require('mongoose');
 
+//config database
+mongoose.connect(config_database.database.connect);
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error:'));
+db.once('open',()=>{
+    console.log('connected to mongodb');
+})
+
+//init app
 const app = express();
 
+//view engine setup
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
+//set public folder
 app.set(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res,next)=>{
-    res.send("<h1>Hello World</h1>");
-});
+
+//set routes
+const pages = require('./routes/page');
+const admin_page = require('./routes/admin_pages');
+
+app.set('/',pages);
+app.set('/',admin_pages);
+
 
 app.listen(config.server.port,()=>{
     console.log('Server running!');
---
+++ configs/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database:{
+        connect:"mongodb+srv://devravache:2002@cluster0-rkghc.mongodb.net/Cluster0?retryWrites=true&w=majority"
+    }
+}

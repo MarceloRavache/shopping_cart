74bda31d4e4e86a110ae2ae67354b12b9df151c5
MarceloRavache <jm.ravache@outlook.com>
2020-06-25 01:45:59 -0300

    models criado, modelo da page criado

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const config = require('./configs/config');
 const config_database = require('./configs/database');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 //config database
 mongoose.connect(config_database.database.connect);
@@ -10,25 +12,45 @@ const db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
 db.once('open',()=>{
     console.log('connected to mongodb');
-})
+});
+
 
 //init app
 const app = express();
+
 
 //view engine setup
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
+
 //set public folder
-app.set(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+//body parser middleware
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}));
+//parse application/json
+app.use(bodyParser.json());
+
+
+//express session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }));
 
 
 //set routes
 const pages = require('./routes/page');
 const admin_page = require('./routes/admin_pages');
+const { Session } = require('inspector');
 
-app.set('/',pages);
-app.set('/admin',admin_page);
+app.use('/',pages);
+app.use('/admin',admin_page);
 
 
 app.listen(config.server.port,()=>{
---
+++ models/pages.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+//page schema
+const PageSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required:true
+    },
+    slog:{
+        type: String
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    sorting:{
+        type: Number
+    }
+});
+
+const Page = module.exports =  mongoose.model('Page',PageSchema);
